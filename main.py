01659401a32b4ef5e42e7f627f0945b9b996de2b
wilfredwilfredkisku <wilfred.kisku@gmail.com>
2020-11-17 13:10:02 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+import torch
+
+#creating containers of pytorch tensors 
+x = torch.empty(5,4)
+y = torch.rand(5,4,3)
+z = torch.tensor([1.1, 2.2])
+print(z)
+
+#
